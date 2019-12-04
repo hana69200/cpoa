ca81ea143d474841c2d8aad351ce7f5c5ca57c96
MIRABEL Marion <marion.mirabel-millot@etu.univ-lyon1.fr>
2019-12-04 17:50:42 +0100

    con sql

--- AssistantPlanning/src/assistantplanning/AssistantPlanning.java
+++ AssistantPlanning/src/assistantplanning/AssistantPlanning.java
@@ -6,7 +6,9 @@
 package assistantplanning;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -17,27 +19,46 @@ import java.sql.Statement;
 public class AssistantPlanning
 {
 
-	/**
-	 * @param args the command line arguments
-	 */
-	public static void main(String[] args)
-	{
-		try
-		 
-		{
-		 
-		Class.forName("com.mysql.jdbc.Driver");
-		 
-		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:81/phone" ,"root",""); //phone et le nom de BD 
-		 
-		Statement statement = connection.createStatement();
-		 
-		System.out.println("Connection Established");
-		 
-		 
-		}catch(SQLException | ClassNotFoundException e){
-		    System.err.println(e);
-		}
-	}
+/**
+* @param args the command line arguments
+*/
+public static void main(String[] args)
+{
+try
+{
+// chargement de la classe par son nom
+Class c = Class.forName("com.mysql.jdbc.Driver");
+Driver pilote = (Driver) c.newInstance();
+// enregistrement du pilote auprès du DriverManager
+DriverManager.registerDriver(pilote);
+// Protocole de connexion
+String protocole = "jdbc:mysql:";
+// Adresse IP de l’hôte de la base et port
+String ip = "https://iutdoua-web.univ-lyon1.fr/phpmyadmin/"; // dépend du contexte
+String port = "3306"; // port MySQL par défaut
+// Nom de la base ;
+String nomBase = "p1607863"; // dépend du contexte
+// Chaîne de connexion
+String conString = protocole + "//" + ip + ":" + port + "/" + nomBase;
+// Identifiants de connexion et mot de passe
+String nomConnexion = "p1607863"; // dépend du contexte
+String motDePasse = "270858"; // dépend du contexte
+// Connexion
+Connection con = DriverManager.getConnection(conString, nomConnexion, motDePasse);
+
+// Envoi d’un requête générique
+String sql = "select * from Joueur";
+Statement smt = con.createStatement();
+ResultSet rs = smt.executeQuery(sql);
+while (rs.next())
+{
+System.out.println(rs.getString("prenom"));
+}
+} catch (Exception e)
+{
+System.out.println(e.getMessage());
+// gestion des exceptions
+}
+}
 
 }
--- AssistantPlanning/src/baseDeDonnee/metierDAO/JoueurDAO.java
+++ AssistantPlanning/src/baseDeDonnee/metierDAO/JoueurDAO.java
@@ -6,6 +6,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import baseDeDonnee.metier.Joueur;
+import java.sql.Driver;
+import java.sql.DriverManager;
 
 public class JoueurDAO
 {
@@ -20,6 +22,27 @@ public class JoueurDAO
 	{
 		List<Joueur> liste = new ArrayList<Joueur>();
 		// TODO acces BD
+                try {
+                    Class c = Class.forName("com.mysql.jdbc.Driver") ;
+                    Driver pilote = (Driver)c.newInstance() ;
+                    DriverManager.registerDriver(pilote);
+                    String protocole =  "jdbc:mysql:" ;
+                    String url =  "//iutdoua-web.univ-lyon1.fr/phpmyadmin/" ;
+                    String port =  "3306" ;
+                    String nomBase =  "p1607863" ;
+                    String nomConnexion =  "p1607863" ;
+                    String motDePasse =  "270858" ;
+                    Connection con = DriverManager.getConnection(conString, nomConnexion, motDePasse) ;
+
+                    String sql =  "select * from Marins" ;
+                    Statement smt = con.createStatement() ;
+                    ResultSet rs = smt.executeQuery(sql) ;
+                     while (rs.next()) {
+                       System.out.println(rs.getString("nom")) ;
+                    }
+                 }  catch (Exception e) {
+                     // gestion des exceptions
+                 }
 		return liste;
 	}
 
